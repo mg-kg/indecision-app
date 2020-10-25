1a07155af876bc571a81edf84249c0c076f80363
KHA <kaunghtetaung10599@gmail.com>
2020-10-25 16:55:48 +0630

    fix add option

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 export default class AddOption extends React.Component {
   state = {
-    error: undefined
+    error: undefined,
   };
   handleAddOption = (e) => {
     e.preventDefault();
@@ -12,13 +12,15 @@ export default class AddOption extends React.Component {
     this.setState(() => ({ error }));
 
     if (!error) {
-      e.target.elements.option.value = '';
+      e.target.elements.option.value = "";
     }
   };
   render() {
     return (
       <div>
-        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+        {this.state.error && (
+          <p className="add-option-error">{this.state.error}</p>
+        )}
         <form className="add-option" onSubmit={this.handleAddOption}>
           <input className="add-option__input" type="text" name="option" />
           <button className="button">Add Option</button>
